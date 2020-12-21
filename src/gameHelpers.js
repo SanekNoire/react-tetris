515f0f60f6fa7939e65491dd13dc96f7f2589adb
Sanek Noire <alexandernoire228@gmail.com>
2020-12-21 14:26:22 +0200

    finish commit

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -2,31 +2,44 @@ export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
 export const createStage = () =>
-	Array.from(Array(STAGE_HEIGHT), () =>
-		new Array(STAGE_WIDTH).fill([0, 'clear'])
-	)
+  Array.from(Array(STAGE_HEIGHT), () => Array(STAGE_WIDTH).fill([0, 'clear']));
 
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
-	for (let y = 0; y < player.tetromino.length; y += 1) {
-		for (let x = 0; x < player.tetromino[y].length; x += 1) {
+  // THIS IS SLOWER!!!
+  // return player.tetromino.some((row, y) =>
+  //   row.some((cell, x) => {
+  //     if (cell !== 0) {
+  //       return (
+  //         !stage[y + player.pos.y + moveY] ||
+  //         !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+  //         stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+  //           'clear'
+  //       );
+  //     }
+  //     return false;
+  //   })
+  // );
 
-			// 1. Check that we're on an actual Tetromino cell
-			if (player.tetromino[y][x] !== 0) {
-				if (
-					// 2. Check that we're within bounds of the stage height (y)
-					// We shouldn't go through the bottom of the play area
-					!stage[y + player.pos.y + moveY] ||
-					// 3. Check that move does not exceed the stage width (x)
-					!stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
-					// 4. Check that the cell we're moving to isn't set to clear
-					stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
-						'clear'
-				) {
-					return true;
-				}
-			}
-
-		}
-	}
-
-}
+  // Using for loops to be able to return (and break). Not possible with forEach
+  for (let y = 0; y < player.tetromino.length; y += 1) {
+    for (let x = 0; x < player.tetromino[y].length; x += 1) {
+      // 1. Check that we're on an actual Tetromino cell
+      if (player.tetromino[y][x] !== 0) {
+        if (
+          // 2. Check that our move is inside the game areas height (y)
+          // That we're not go through bottom of the play area
+          !stage[y + player.pos.y + moveY] ||
+          // 3. Check that our move is inside the game areas width (x)
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          // 4. Check that the cell wer'e moving to isn't set to clear
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+            'clear'
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+  // 5. If everything above is false
+  return false;
+};
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,58 +1,56 @@
 import { useState, useCallback } from 'react';
 
-import { TETROMINOES, randomTetromino } from '../tetrominoes';
+import { TETROMINOS, randomTetromino } from '../tetrominos';
 import { STAGE_WIDTH, checkCollision } from '../gameHelpers';
 
 export const usePlayer = () => {
-	const [ player, setPlayer ] = useState({
-		pos: { x: 0, y: 0 },
-		tetromino: TETROMINOES[0].shape,
-		collided: false,
-	});
+  const [player, setPlayer] = useState({
+    pos: { x: 0, y: 0 },
+    tetromino: TETROMINOS[0].shape,
+    collided: false,
+  });
 
-	const rotate = (matrix, dir) => {
-		// transpose rows to cols
-		const rotatedTetro = matrix.map((_, index) => 
-			matrix.map(col => col[index]),
-		);
-		// Reverse each row to get a rotated matrix
-		if(dir > 0) return rotatedTetro.map(row => row.reverse());
-		return rotatedTetro.reverse();
-	}
+  function rotate(matrix, dir) {
+    // Make the rows to become cols (transpose)
+    const mtrx = matrix.map((_, index) => matrix.map(column => column[index]));
+    // Reverse each row to get a rotaded matrix
+    if (dir > 0) return mtrx.map(row => row.reverse());
+    return mtrx.reverse();
+  }
 
-	const playerRotate = (stage, dir) => {
-		const clonedPlayer = JSON.parse(JSON.stringify(player));
-		clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+  function playerRotate(stage, dir) {
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
 
-		const pos = clonedPlayer.pos.x;
-		let offset = 1;
-		while(checkCollision(clonedPlayer, stage, { x: 0, y: 0})){
-			clonedPlayer.pos.x += offset;
-			offset = -(offset + (offset > 0 ? 1 : -1));
-			if (offset > clonedPlayer.tetromino[0].length){
-				rotate(clonedPlayer.tetromino, -dir);
-				clonedPlayer.pos.x = pos;
-			}
-		}
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+    setPlayer(clonedPlayer);
+  }
 
-		setPlayer(clonedPlayer);
-	}
+  const updatePlayerPos = ({ x, y, collided }) => {
+    setPlayer(prev => ({
+      ...prev,
+      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
+      collided,
+    }));
+  };
 
-	const updatePlayerPos = ({ x, y, collided }) => {
-		setPlayer(prev => ({
-			...prev,
-			pos: { x: (prev.pos.x += x), y: (prev.pos.y += y)},
-			collided,
-		}))
-	}
+  const resetPlayer = useCallback(() => {
+    setPlayer({
+      pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+      tetromino: randomTetromino().shape,
+      collided: false,
+    });
+  }, []);
 
-	const resetPlayer = useCallback(() => {
-		setPlayer({
-			pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
-			tetromino: randomTetromino().shape,
-			collided: false,
-		})
-	}, [])
-
-	return [ player, updatePlayerPos, resetPlayer, playerRotate ];
-}
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
+};
